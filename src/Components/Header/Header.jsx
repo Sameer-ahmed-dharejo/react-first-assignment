b332a447b53f47d5faae8a120da10b9292a26f36
unknown <sameerahmeddharejo.786@gmail.com>
2024-10-31 09:28:31 +0500

    Added class name nav and footer

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,27 +4,27 @@ const Header = () =>{
     return(
         <>
 <div className="Header vh-100 img-fluid">
-<nav class="navbar navbar-expand-lg container">
-  <div class="container-fluid">
-    <a class="navbar-brand text-white" href="#">MNTN</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg container">
+  <div className="container-fluid">
+    <a className="navbar-brand text-white" href="#">MNTN</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav m-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link text-white" aria-current="page" href="#">Equipment</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link text-white" aria-current="page" href="#">Equipment</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="#">About Us</a>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">About Us</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-white">Blog</a>
+        <li className="nav-item">
+          <a className="nav-link text-white">Blog</a>
         </li>
       </ul>
-      <div class="d-flex text-white">
+      <div className="d-flex text-white">
      
-      <h6> <Icon class="btn-group-vertical me-1" icon="ic:outline-account-circle" />Account</h6>
+      <h6> <Icon className="btn-group-vertical me-1" icon="ic:outline-account-circle" />Account</h6>
       </div>
     </div>
   </div>
